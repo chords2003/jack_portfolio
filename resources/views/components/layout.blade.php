c3d15b3dfd6397cbc3cc40542abca75dcd6326c3
chords2003 <chords2003@yahoo.com>
2024-05-12 17:55:51 -0400

    created layout page

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,19 +6,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Portfolio</title>
-    @vite(['resources/js/app.js'])
+    @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
 
-<body>
-    <div>
-        <nav>
+<body class="bg-black text-white border-b border-white/10">
+    <div class="px-10">
+        <nav class="flex justify-between items-center  py-4">
             <div>
                 <a href="">
-                    <img src="" alt="">
+                    <img src="{{ Vite::asset('resources/images/logo.svg')  }}" alt="">
                 </a>
             </div>
-            <div>Nav Links bar</div>
-            <div>profile</div>
+            <div>
+                <a href="">Home</a>
+                <a href="">About</a>
+                <a href="">Projects</a>
+                <a href="">Contact</a>
+            </div>
+            <div>
+                <a href="">Login</a>
+                <a href="">Register</a>
+            </div>
         </nav>
         <main>
             {{ $slot }}

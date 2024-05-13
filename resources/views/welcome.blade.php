f84091c9bdebc1da32a53472c6b47acc0bbcceef
chords2003 <chords2003@yahoo.com>
2024-05-13 02:15:30 -0400

    added sections for app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,26 +2,30 @@
 <div class="space-y-10">
     <section>
         <x-section-heading>
-            <h2>Featured Jobs</h2>
+            Featured Jobs
         </x-section-heading>
         <div class="grid lg:grid-cols-3 gap-8 mt-6">
             <x-job-card />
             <x-job-card />
             <x-job-card />
-
         </div>
     </section>
-    <section>
 
+    <section>
         <x-section-heading>
-            <h2>Tags</h2>
+           Tags
         </x-section-heading>
     </section>
-    <section>
 
-        <x-section-heading>
-            <h2>Recent Jobs</h2>
-        </x-section-heading>
+    <section>
+        <x-section-heading>Recent Jobs</x-section-heading>
+            <div class="mt-6 space-x-1">
+                <x-tag>PHP</x-tag>
+                <x-tag>PHP</x-tag>
+                <x-tag>PHP</x-tag>
+                <x-tag>PHP</x-tag>
+                <x-tag>PHP</x-tag>
+            </div>
     </section>
 </div>
 

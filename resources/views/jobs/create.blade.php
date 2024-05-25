e2ea9253e78eb449bc4c52cb70fdc4d55824115b
chords2003 <chords2003@yahoo.com>
2024-05-25 16:39:19 -0400

    Added the form to create a job post

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -4,7 +4,15 @@
         <x-input :label="false" name="title" placeholder="Title" />
         <x-input :label="false" name="salary" placeholder="Salary" />
         <x-input :label="false" name="location" placeholder="Location" />
-        <x-input :label="false" name="tags" placeholder="Tags" />
-        <x-button>Create Job</x-button>
+        <x-select :label="false" name="schedule">
+            <option value="Full-time">Full-time</option>
+            <option value="Part-time">Part-time</option>
+            <option value="Contract">Contract</option>
+        </x-select>
+        <x-input :label="false" name="url" placeholder="https://acme.com/jobs/ceo-wanted" />
+        <x-checkbox :label="false" name="feature" />
+
+        <x-input :label="false" name="tags" placeholder="Laracast, Video, education" />
+        <x-button>Publish Job</x-button>
     </x-form>
 </x-layout>

5c06c9469ceb58ab1809e8d1f551649edb40cc15
chords2003 <chords2003@yahoo.com>
2024-05-26 12:55:22 -0400

    Added the form to create jobs and fixed labeling issue

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -1,18 +1,18 @@
 <x-layout>
     <x-page-heading>Create a new job</x-page-heading>
     <x-form action="/jobs" method="POST" class="mt-6">
-        <x-input :label="false" name="title" placeholder="Title" />
-        <x-input :label="false" name="salary" placeholder="Salary" />
-        <x-input :label="false" name="location" placeholder="Location" />
-        <x-select :label="false" name="schedule">
+        <x-input label="Title" name="title" placeholder="Title" />
+        <x-input label="Salary" name="salary" placeholder="Salary" />
+        <x-input label="Location" name="location" placeholder="Location" />
+        <x-select label="Schedule" name="schedule">
             <option value="Full-time">Full-time</option>
             <option value="Part-time">Part-time</option>
             <option value="Contract">Contract</option>
         </x-select>
-        <x-input :label="false" name="url" placeholder="https://acme.com/jobs/ceo-wanted" />
-        <x-checkbox :label="false" name="feature" />
-
-        <x-input :label="false" name="tags" placeholder="Laracast, Video, education" />
+        <x-input label="URL" name="url" placeholder="https://acme.com/jobs/ceo-wanted" />
+        <x-checkbox label="Feature (Cost Extra)" name="feature" />
+        <x-divider />
+        <x-input label="Tags (comma separated)" name="tags" placeholder="Laracast, Video, education" />
         <x-button>Publish Job</x-button>
     </x-form>
 </x-layout>
